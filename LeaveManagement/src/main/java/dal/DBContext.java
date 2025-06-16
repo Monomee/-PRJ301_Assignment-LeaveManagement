6c963cbff8862fa0de131b7d4995c9a5738f5e1e
Monomee <hoangthth12@gmail.com>
2025-06-16 12:09:19 +0700

    base

---
+++ LeaveManagement/src/main/java/dal/DBContext.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ *
+ * @author PC
+ */
+public abstract class DBContext {
+    private final EntityManagerFactory entityManagerFactory;
+
+    public DBContext() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("nvh_persistance");
+
+    }
+
+    protected EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+}
